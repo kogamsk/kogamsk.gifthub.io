c22b8ead017742ee38b9326651df3a510f1bfc83
kogamsk <mleftair@gmail.com>
2025-04-21 16:06:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleDetails() {
-  const details = document.getElementById("details");
-  details.classList.toggle("hidden");
-}
+// 「連絡する」ボタンをクリックした際にアラートを表示
+document.getElementById('contact-btn').addEventListener('click', () => {
+  alert('お気軽にご連絡ください！吹奏楽の話を楽しみにしています！');
+});
